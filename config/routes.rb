8590939821ab87f77c9ea2671de11708d8c50ccc
jduffel1929@hotmail.com <jduffel1929@hotmail.com>
2020-07-23 00:54:44 +0000

    Implement add && list categories

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+
   
   resources :orders, only: [:create, :show]
   
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :categories, :products, except: [:edit, :update, :show]
   end
   
   get "/about" => "about#show"
